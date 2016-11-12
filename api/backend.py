2f243acae913aeeefac20e6cb5184802cda160d1
Andrew Thomas <andrew@cjthomas.org>
2016-11-12 16:33:30 -0600

    integrating capital one api

--- api/backend.py
+++ api/backend.py
@@ -3,7 +3,10 @@ import os
 import urlparse
 import uuid
 from flask import *
+from secret import *
 import psycopg2
+import requests
+import json
 
 urlparse.uses_netloc.append("postgres")
 URL = urlparse.urlparse(os.environ["DATABASE_URL"])
@@ -22,49 +25,50 @@ app = Flask(__name__)
 @app.route('/user', methods=['POST'])
 def generate_user():
     """Generate a new user and return the """
-    try:
-        # The data will be stored in request.form['key']
-        # retrieve from there and send response in
-        form = request.json
-        phone_num = form['phone']
-        first_name = form['account_info']['first']
-        last_name = form['account_info']['last']
-        zip_code = form['account_info']['zip']
 
-        # Use the Nessie API to figure out their id using first/last/zip
+    # The data will be stored in request.form['key']
+    # retrieve from there and send response in
+    form = request.json
+    phone_num = form['phone']
+    first_name = form['account_info']['first']
+    last_name = form['account_info']['last']
+    zip_code = form['account_info']['zip']
 
-        nessie_api_id = ""
-        auth_token = uuid.uuid4()
+    # Use the Nessie API to figure out their id using first/last/zip
 
-        # insert the data into the database
-        cur = CONN.cursor()
-        cur.execute('INSERT INTO Users Values (\'{0}\', \'{1}\', \'{2}\');'.format(
-            str(auth_token), nessie_api_id, phone_num))
-        CONN.commit()
+    customers = get_request('/customers')
+    customers = [x for x in customers if (x['first_name'] == first_name and x['last_name'] == last_name) \
+        and x['address']['zip'] == zip_code]
 
-        response = make_response('{"auth_token":'+'"{0}"'.format(str(auth_token))+'}')
-        response.headers['Content-Type'] = 'application/json'
+    nessie_api_id = customers[0]['_id']
+    auth_token = uuid.uuid4()
 
-        return response
-    except Exception as err:
-        return err.message
+    # insert the data into the database
+    cur = CONN.cursor()
+    cur.execute('INSERT INTO Users Values (\'{0}\', \'{1}\', \'{2}\');'.format(
+        str(auth_token), nessie_api_id, phone_num))
+    CONN.commit()
+
+    response = make_response('{"auth_token":'+'"{0}"'.format(str(auth_token))+'}')
+    response.headers['Content-Type'] = 'application/json'
+
+    return response
 
 @app.route('/location', methods=['POST'])
 def locationUpdate():
     """The user has given us a new data point. see if they are close to a bad place and text them"""
-    try:
-        # get the data out of the request
-        form = request.json
-        auth_token = form['auth_token']
-        lat, lng = form['lat'], form['lng']
+    
+    # get the data out of the request
+    form = request.json
+    auth_token = form['auth_token']
+    lat, lng = form['lat'], form['lng']
 
-        list_of_places = get_popular_locations_near_me(auth_token, lat, lng)
+    list_of_places = get_popular_locations_near_me(auth_token, lat, lng)
 
-        # Now, we want to somehow query data around the User
-        # if they are close to any, send a twilio message
-        return 'Heartbeat'
-    except Exception as err:
-        return str(err) + ' Ya done messed up'
+    # Now, we want to somehow query data around the User
+    # if they are close to any, send a twilio message
+    return
+    
 
 @app.route('/aroundme', methods=['POST'])
 def sendLocations():
@@ -103,6 +107,19 @@ def get_popular_locations_near_me(auth_token, lat, lng):
     list_of_places = []
 
     return list_of_places
+
+def get_url(path):
+    return '{0}{1}?key={2}'.format(BASE_API, path, API_KEY)
+
+def get_request(path):
+    return requests.get(get_url(path)).json()
+
+def post_request(path, payload):
+    return requests.post(
+        get_url(path),
+        data=json.dumps(payload),
+        headers={'Content-Type': 'application/json'}
+    )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
