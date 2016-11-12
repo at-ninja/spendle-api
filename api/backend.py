76fb3d3be7141b86d98bd4c8832d6a9dc1b6be1a
Andrew Thomas <andrew@cjthomas.org>
2016-11-12 17:42:51 -0600

    trying

--- api/backend.py
+++ api/backend.py
@@ -109,7 +109,7 @@ def get_popular_locations_near_me(auth_token, lat, lng):
     cur.execute('SELECT nesie_id FROM Users WHERE id = \'{0}\';'.format(
         str(auth_token)))
 
-     nessie_id = ''.join(cur.fetchone())
+    nessie_id = ''.join(cur.fetchone())
     
     list_of_merchants = get_request('/merchants', params={'lat':lat, 'lng':lng})
 
